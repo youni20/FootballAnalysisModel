34ab2ce8f2da731c01aad1643d434a9c968425b0
youni20 <moyounus20@gmail.com>
2025-04-12 19:53:18 +0200

    Trained Models

--- yolo_interface.py
+++ yolo_interface.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8x")
+model = YOLO("models/best.pt")
 results = model.predict("input_videos/messiwinner.mp4", save=True)
 # Results will contain detections
 
